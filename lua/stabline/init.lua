c1cff73dcf29097ca92319516fee0821ebe0595c
Andrew Cooke <andrew.dylan.cooke@gmail.com>
2022-12-30 15:54:36 -0500

    fixes

--- lua/stabline/init.lua
+++ lua/stabline/init.lua
@@ -3,15 +3,15 @@ local M = {}
 local config = require("stabline.config")
 
 M.statusline = function()
-  require("stabline.statusline").run(config.statusline)
+  return require("stabline.statusline").run(config.statusline)
 end
 
 M.tabline = function()
-  require("stabline.statusline").run(config.statusline)
+  return require("stabline.statusline").run(config.statusline)
 end
 
 M.setup = function()
-  vim.opt.statusline = "%!v:helloworld"
+  vim.opt.statusline = "%!v:lua.require('stabline').statusline()"
 end
 
 return M
