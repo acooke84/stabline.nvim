c76fbc773283ae3555a51ce5eb722137bd17ff4a
Andrew Cooke <andrew.dylan.cooke@gmail.com>
2022-12-30 22:22:37 -0500

    fileInfo

--- lua/stabline/statusline/init.lua
+++ lua/stabline/statusline/init.lua
@@ -4,7 +4,6 @@ return {
 
     return table.concat {
       modules.mode(),
-      "Hello, World!",
     }
   end
 }
--- lua/stabline/statusline/modules.lua
+++ lua/stabline/statusline/modules.lua
@@ -1,33 +1,32 @@
 local sep_r = " "
 
 local modes = {
-  ["n"] = { "NORMAL", "St_NormalMode" },
-  ["niI"] = { "NORMAL i", "St_NormalMode" },
-  ["niR"] = { "NORMAL r", "St_NormalMode" },
-  ["niV"] = { "NORMAL v", "St_NormalMode" },
-  ["no"] = { "N-PENDING", "St_NormalMode" },
-  ["i"] = { "INSERT", "St_InsertMode" },
-  ["ic"] = { "INSERT (completion)", "St_InsertMode" },
-  ["ix"] = { "INSERT completion", "St_InsertMode" },
-  ["t"] = { "TERMINAL", "St_TerminalMode" },
-  ["nt"] = { "NTERMINAL", "St_NTerminalMode" },
-  ["v"] = { "VISUAL", "St_VisualMode" },
-  ["V"] = { "V-LINE", "St_VisualMode" },
-  ["Vs"] = { "V-LINE (Ctrl O)", "St_VisualMode" },
-  [""] = { "V-BLOCK", "St_VisualMode" },
-  ["R"] = { "REPLACE", "St_ReplaceMode" },
-  ["Rv"] = { "V-REPLACE", "St_ReplaceMode" },
-  ["s"] = { "SELECT", "St_SelectMode" },
-  ["S"] = { "S-LINE", "St_SelectMode" },
-  [""] = { "S-BLOCK", "St_SelectMode" },
-  ["c"] = { "COMMAND", "St_CommandMode" },
-  ["cv"] = { "COMMAND", "St_CommandMode" },
-  ["ce"] = { "COMMAND", "St_CommandMode" },
-  ["r"] = { "PROMPT", "St_ConfirmMode" },
-  ["rm"] = { "MORE", "St_ConfirmMode" },
-  ["r?"] = { "CONFIRM", "St_ConfirmMode" },
-  ["!"] = { "SHELL", "St_TerminalMode" },
-  ["t"] = { "TERMINAL", "St_TerminalMode" },
+  ["n"] = { "NORMAL", "Stab_NormalMode" },
+  ["niI"] = { "NORMAL i", "Stab_NormalMode" },
+  ["niR"] = { "NORMAL r", "Stab_NormalMode" },
+  ["niV"] = { "NORMAL v", "Stab_NormalMode" },
+  ["no"] = { "N-PENDING", "Stab_NormalMode" },
+  ["i"] = { "INSERT", "Stab_InsertMode" },
+  ["ic"] = { "INSERT (completion)", "Stab_InsertMode" },
+  ["ix"] = { "INSERT completion", "Stab_InsertMode" },
+  ["t"] = { "TERMINAL", "Stab_TerminalMode" },
+  ["nt"] = { "NTERMINAL", "Stab_NTerminalMode" },
+  ["v"] = { "VISUAL", "Stab_VisualMode" },
+  ["V"] = { "V-LINE", "Stab_VisualMode" },
+  ["Vs"] = { "V-LINE (Ctrl O)", "Stab_VisualMode" },
+  [""] = { "V-BLOCK", "Stab_VisualMode" },
+  ["R"] = { "REPLACE", "Stab_ReplaceMode" },
+  ["Rv"] = { "V-REPLACE", "Stab_ReplaceMode" },
+  ["s"] = { "SELECT", "Stab_SelectMode" },
+  ["S"] = { "S-LINE", "Stab_SelectMode" },
+  [""] = { "S-BLOCK", "Stab_SelectMode" },
+  ["c"] = { "COMMAND", "Stab_CommandMode" },
+  ["cv"] = { "COMMAND", "Stab_CommandMode" },
+  ["ce"] = { "COMMAND", "Stab_CommandMode" },
+  ["r"] = { "PROMPT", "Stab_ConfirmMode" },
+  ["rm"] = { "MORE", "Stab_ConfirmMode" },
+  ["r?"] = { "CONFIRM", "Stab_ConfirmMode" },
+  ["!"] = { "SHELL", "Stab_TerminalMode" },
 }
 
 local M = {}
@@ -35,9 +34,15 @@ local M = {}
 M.mode = function()
   local m = vim.api.nvim_get_mode().mode
   local current_mode = "%#" .. modes[m][2] .. "#" .. "  " .. modes[m][1]
-  local mode_sep1 = "%#" .. modes[m][2] .. "Sep" .. "#" .. sep_r
 
-  return current_mode .. mode_sep1 .. "%#ST_EmptySpace#" .. sep_r
+  return current_mode .. sep_r
+end
+
+M.fileInfo = function()
+  local icon = "  "
+  local filename = (fn.expand("%") == "" and "Empty ") or fn.expand("%:t")
+
+  return "%#Stab_FileInfo#" .. icon .. filename .. sep_r
 end
 
 return M
